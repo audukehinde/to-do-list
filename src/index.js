ade111299f9ff2be5e1a5f3a9e1c195df93a1647
unknown <auduomohkehinde@gmail.com>
2023-01-17 04:15:47 -0800

    Added webpack to project

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { camelCase } from 'lodash';
+import './style.css';
+
+console.log(camelCase('Hello World!'));
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    // contentBase: path.resolve(__dirname, 'public'),
+    // port: 9000,
+    static: './dist',
+  },
+};
